a6d423f44e3717f137404ef87f54069d2faa15f3
Ethan Sagin <ethansagin@gmail.com>
2019-05-23 16:57:49 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,49 @@
+class Dog 
+  attr_accessor :id, :name, :breed 
+  
+  def initialize(name:, breed:, id: nil)
+    @name = name
+    @breed = breed
+  end
+  
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE dogs (id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT)
+    SQL
+    DB[:conn].execute(sql)
+  end
+  
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE dogs
+    SQL
+    DB[:conn].execute(sql)
+  end
+  
+  def save(inst)
+    sql = <<-SQL
+      INSERT INTO dogs (name, breed)
+      VALUES (?, ?)
+    SQL
+    
+  end
+  
+  def self.new_from_db(row)
+    self.new(name: row[1], breed: row[2]).tap do |dog|
+      dog.id = row[0]
+    end
+  end
+  
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * FROM dogs WHERE Dogs.name = ?
+    SQL
+    
+    row = DB[:conn].execute(sql, name)
+    
+    self.new_from_db(row)
+  end
+  
+end
