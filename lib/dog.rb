057b517efd831b9f694cc341d2560c849458450b
Ethan Sagin <ethansagin@gmail.com>
2019-05-23 17:11:45 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -22,11 +22,13 @@ class Dog
     DB[:conn].execute(sql)
   end
   
-  def save(inst)
+  def save
     sql = <<-SQL
       INSERT INTO dogs (name, breed)
       VALUES (?, ?)
     SQL
+    
+    DB[:conn].execute(sql, self.name, self.breed)
     
   end
   
